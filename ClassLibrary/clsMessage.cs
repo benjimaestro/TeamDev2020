ac3038ff5a9aa6ba208e0cb679a75aabe390d64b
Benjamin Jones <benjamin.gjones@yahoo.co.uk>
2021-01-19 13:20:50 +0000

    More classes started

---
+++ ClassLibrary/clsMessage.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    class clsMessage
+    {
+        private Int32 mID;
+        private Int32 mFromID;
+        private Int32 mToID;
+        private string mContent;
+        private DateTime mTimestamp;
+
+        public int ID
+        {
+            get { return mID; }
+            set { mID = value; }
+        }
+        public int FromID
+        {
+            get { return mFromID; }
+            set { mFromID = value; }
+        }
+        public int ToID
+        {
+            get { return mToID; }
+            set { mToID = value; }
+        }
+        public string Content
+        {
+            get { return mContent; }
+            set { mContent = value; }
+        }
+        public DateTime Timestamp
+        {
+            get { return mTimestamp; }
+            set { mTimestamp = value; }
+        }
+        public bool Find(int ID)
+        {
+            //FINISH MEEEEEEEEEEEEEEE
+            return true;
+        }
+        public string Validate(int ID, int FromID, int ToID, int Content, DateTime Timestamp)
+        {
+            //Finish me also!!!!!!!
+            string Error = "";
+            return Error;
+        }
+    }
+}
---
+++ ClassLibrary/clsRoom.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    class clsRoom
+    {
+        private Int32 mID;
+        private Int32 mNumber;
+        private string mBlock;
+        private string mSubject;
+
+        public int ID
+        {
+            get { return mID; }
+            set { mID = value; }
+        }
+        public int Number
+        {
+            get { return mNumber; }
+            set { mNumber = value; }
+        }
+        public string Block
+        {
+            get { return mBlock; }
+            set { mBlock = value; }
+        }
+        public string Subject
+        {
+            get { return mSubject; }
+            set { mSubject = value; }
+        }
+        public bool Find(int ID)
+        {
+            //FINISH MEEEEEEEEEEEEEEE
+            return true;
+        }
+        public string Validate(int ID, int Number, string Block, int Subject)
+        {
+            //Finish me also!!!!!!!
+            string Error = "";
+            return Error;
+        }
+    }
+}
