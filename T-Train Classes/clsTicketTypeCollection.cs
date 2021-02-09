c7f201f01ddcc8f48a713405d923c07b20242d74
LanternCode <Adam.Machowczyk@gmail.com>
2021-02-09 10:30:37 +0000

    Added property testing for Ticket Type collection class as well as the class itself

---
+++ T-Train Classes/clsTicketTypeCollection.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace T_Train_Classes
+{
+    public class clsTicketTypeCollection
+    {
+        public List<clsTicketType> MyTicketTypes { get; set; }
+        public clsTicketType ThisTicketType { get; set; }
+        public int Count
+        {
+            get
+            {
+                return MyTicketTypes.Count;
+            }
+            set
+            {
+                //we will do this later
+            }
+        }
+    }
+}
