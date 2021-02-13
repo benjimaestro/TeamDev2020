7eb984dd96b2ce4b514eb7d60d1b26b0e5542686
LanternCode <Adam.Machowczyk@gmail.com>
2021-02-13 13:45:49 +0000

    Added CRUS and unit testing to collection class for ticket type.

--- T-Train Classes/clsTicketTypeCollection.cs
+++ T-Train Classes/clsTicketTypeCollection.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace T_Train_Classes
 {
@@ -16,6 +17,43 @@ namespace T_Train_Classes
             {
                 //we will do this later
             }
+        }
+
+        public int AddTicketType()
+        {
+            //connect to the database
+            clsDataConnection DB = new clsDataConnection();
+            //set the parameters for the stored procedure
+            DB.AddParameter("@ticketTypeActive", ThisTicketType.TicketTypeActive);
+            DB.AddParameter("@ticketTypeName", ThisTicketType.TicketTypeName);
+            DB.AddParameter("@ticketTypePrice", ThisTicketType.TicketTypePrice);
+            DB.AddParameter("@ticketTypeRefundable", ThisTicketType.TicketTypeRefundable);
+            //execute the query returning the primary key value
+            return DB.Execute("sproc_tblTicketType_Insert");
+        }
+
+        public void DeleteTicketType()
+        {
+            //connect to the database
+            clsDataConnection DB = new clsDataConnection();
+            //add the only parameter which is the id of customer to delete
+            DB.AddParameter("@TicketTypeId", ThisTicketType.TicketTypeId);
+            //proceed to delete the staff member
+            DB.Execute("sproc_tblTicketType_Delete");
+        }
+
+        public void ModifyTicketType()
+        {
+            //connect to the database
+            clsDataConnection DB = new clsDataConnection();
+            //set the parameters for the stored procedure
+            DB.AddParameter("@TicketTypeActive", ThisTicketType.TicketTypeActive);
+            DB.AddParameter("@TicketTypeName", ThisTicketType.TicketTypeName);
+            DB.AddParameter("@TicketTypePrice", ThisTicketType.TicketTypePrice);
+            DB.AddParameter("@TicketTypeRefundable", ThisTicketType.TicketTypeRefundable);
+            DB.AddParameter("@TicketTypeId", ThisTicketType.TicketTypeId);
+            //update the record
+            DB.Execute("sproc_tblTicketType_Update");
         }
     }
 }
--- T-Train Testing/tstClsTicketTypeCollection.cs
+++ T-Train Testing/tstClsTicketTypeCollection.cs
@@ -86,5 +86,107 @@ namespace TTrainTicketType
             //test to see that the two values are the same
             Assert.AreEqual(ATicketTypeCollection.Count, testList.Count);
         }
+
+        [TestMethod]
+        public void AddMethodExists()
+        {
+            //create an instance of the class we want to create
+            clsTicketTypeCollection ATicketTypeCollection = new clsTicketTypeCollection();
+            //a test object
+            clsTicketType ATicketType = new clsTicketType
+            {
+                //assign all the properties
+                TicketTypeActive = true,
+                TicketTypeId = 15,
+                TicketTypeName = "Summer Special 2021",
+                TicketTypePrice = 12.75f,
+                TicketTypeRefundable = false
+            };
+            //set ThisStaff to the test data
+            ATicketTypeCollection.ThisTicketType = ATicketType;
+            //store the primary key
+            //add the record
+            int primaryKey = ATicketTypeCollection.AddTicketType();
+            //set the primary key of the test data
+            ATicketType.TicketTypeId = primaryKey;
+            //find the record
+            ATicketTypeCollection.ThisTicketType.FindTicketType(primaryKey);
+            //test to see that the two values are the same
+            Assert.AreEqual(ATicketTypeCollection.ThisTicketType, ATicketType);
+            //delete the recod not to fill the database with duplicate records
+            ATicketTypeCollection.DeleteTicketType();
+        }
+
+        [TestMethod]
+
+        public void DeleteMethodOK()
+        {
+            //create an instance of the class we want to create
+            clsTicketTypeCollection ATicketTypeCollection = new clsTicketTypeCollection();
+            //a test object
+            clsTicketType ATicketType = new clsTicketType
+            {
+                //assign all the properties
+                TicketTypeActive = true,
+                TicketTypeId = 15,
+                TicketTypeName = "Summer Special 2021",
+                TicketTypePrice = 12.75f,
+                TicketTypeRefundable = false
+            };
+            //set ThisStaff to the test data
+            ATicketTypeCollection.ThisTicketType = ATicketType;
+            //store the primary key
+            //add the record
+            int primaryKey = ATicketTypeCollection.AddTicketType();
+            //set the primary key of the test data
+            ATicketType.TicketTypeId = primaryKey;
+            //find the record
+            ATicketTypeCollection.ThisTicketType.FindTicketType(primaryKey);
+            //delete the record
+            ATicketTypeCollection.DeleteTicketType();
+            //now find the record
+            bool found = ATicketTypeCollection.ThisTicketType.FindTicketType(primaryKey);
+            //the record must not be found
+            Assert.IsFalse(found);
+        }
+
+        [TestMethod]
+        public void UpdateMethodOK()
+        {
+            //create an instance of the class we want to create
+            clsTicketTypeCollection ATicketTypeCollection = new clsTicketTypeCollection();
+            //a test object
+            clsTicketType ATicketType = new clsTicketType
+            {
+                //assign all the properties
+                TicketTypeActive = true,
+                TicketTypeId = 15,
+                TicketTypeName = "Summer Special 2021",
+                TicketTypePrice = 12.75f,
+                TicketTypeRefundable = false
+            };
+            //set ThisStaff to the test data
+            ATicketTypeCollection.ThisTicketType = ATicketType;
+            //store the primary key
+            //add the record
+            int primaryKey = ATicketTypeCollection.AddTicketType();
+            //set the primary key of the test data
+            ATicketType.TicketTypeId = primaryKey;
+            //assign all the properties
+            ATicketType.TicketTypeActive = false;
+            ATicketType.TicketTypeName = "Super Summer Special 2021";
+            ATicketType.TicketTypePrice = 15.75f;
+            ATicketType.TicketTypeRefundable = true;
+            //assign the test object to the real object
+            ATicketTypeCollection.ThisTicketType = ATicketType;
+            //update data of the real object
+            ATicketTypeCollection.ModifyTicketType();
+            //find the record
+            ATicketTypeCollection.ThisTicketType.FindTicketType(primaryKey);
+            //check if the data matches
+            Assert.AreEqual(ATicketTypeCollection.ThisTicketType, ATicketType);
+            //delete the record not to fill the database with duplicate records
+            ATicketTypeCollection.DeleteTicketType();
+        }
     }
 }
